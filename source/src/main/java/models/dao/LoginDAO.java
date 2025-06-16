61e9d90f656e66f94bc1dd35c37d64c95c6bb4a0
plusdojo2025-akamine <akamine-kota-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 12:20:57 +0900

    ファイル構成追加

---
+++ source/src/main/java/models/dao/LoginDAO.java
@@ -0,0 +1,10 @@
+package models.dao;
+
+public class LoginDAO {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ source/src/main/java/models/dto/Users.java
@@ -0,0 +1,10 @@
+package models.dto;
+
+public class Users {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
