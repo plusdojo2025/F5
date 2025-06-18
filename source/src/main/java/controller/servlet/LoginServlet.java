9f1a5263a731b59c8d514b6549e778b28c2cf2b2
plusdojo2025-kishi <kishi-yuka-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 14:38:20 +0900

    Login.Servletとhome.jspとlogin.jspの追加

--- source/src/main/java/controller/servlet/LoginServlet.java
+++ source/src/main/java/controller/servlet/LoginServlet.java
@@ -1,11 +1,17 @@
-package Controller.servlet;
+package controller.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import models.dao.LoginDAO;
+import models.dto.Users;
 
 /**
  * Servlet implementation class LoginServlet
@@ -14,28 +20,47 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+				// ログインページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/users/login.jsp");
+				dispatcher.forward(request, response);
+			}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		// ログイン処理を行う
+		
+		
+		LoginDAO iDao = new LoginDAO();
+		if (iDao.isLoginOK(new Users(0,password,"",email,null,"",""))) { // ログイン成功
+				// セッションスコープにメールアドレスを格納する
+				HttpSession session = request.getSession();
+				//session.setAttribute("id", new LoginUser(id));//
+				session.setAttribute("email", email);
+
+				// トップページにリダイレクトする
+				response.sendRedirect("/WEB-INF/controller/servlet/TopServlet");
+			} else { // ログイン失敗
+					// リクエストスコープに、タイトル、メッセージ、戻り先を格納する
+					request.setAttribute("result", "/WEB-INF/controller/servlet/Login.Servlet");
+			
+			// 結果ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/users/home.jsp");
+			dispatcher.forward(request, response);
+			
+			}
 	}
 
 }
---
+++ source/src/main/webapp/WEB-INF/jsp/users/login.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>せんたくびより</title>
+    <link rel="stylesheet" href="css/common.css">
+</head>
+
+<body>
+<!-- ヘッダーここから -->
+<header class="header">
+     <div class="title-top">
+        <h1 class="main-title">せんたくびより</h1>
+        <h2 class="subtitle">～wash of life～</h2>
+    </div>
+</header>
+<!-- ヘッダーここまで -->
+
+<!-- メインここから --> 
+<main>
+<div class="login_form">  
+    <section>
+        <form id="form" action="/F5/LoginServlet" method="POST">
+            <div class="login_form_btm">
+                <div class="form_row">
+                    <label for="id">メールアドレス</label>
+                    <input type="email" name="id" id="id" placeholder="メールアドレスを入力してください">
+                </div>
+                <div class="form_row">
+                    <label for="pw">パスワード</label>
+                    <input type="password" name="pw" id="pw" placeholder="パスワードを入力してください">
+                </div>
+            </div>
+            <button type="submit">ログイン</button>
+            <p id="output" style="color: #4A3E37;"></p> <!-- エラーメッセージ表示用 -->
+        </form>
+  </section>
+</div>
+<div class="user_regist_button">
+    <a href="user_regist.jsp">新規登録はこちら</a>
+</div>
+</main>
+<!-- メインここまで -->
+
+<!-- フッターここから -->
+<footer>
+    <p class="copyright">&copy; せんたくびより</p>
+</footer>
+<!-- フッターここまで -->
+
+</body>
+</html>
